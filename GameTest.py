e4f72389bc09dc3e824754f08e09b46d74d818ce
GnomishGalaxy <dan.korzycki@gmail.com>
2025-05-04 14:05:38 +0200

    did some tests

--- GameTest.py
+++ GameTest.py
@@ -5,6 +5,13 @@ from main import *
 @pytest.fixture
 def board():
     return Board(CreateDeck())
+random.seed(42)
 
 def test_printcards(board):
-    assert display(board)
+    assert str(board.deck[0]) == "(3, ♥)"
+
+def test_turn(board):
+    board = turn(board)
+    assert board.face[0] == Card
+    assert board.face[1] == Card
+    assert board.face[2] == Card
